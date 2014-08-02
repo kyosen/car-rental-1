a9ab18c6800faa16c5694630f28f5ab242d3b10b
Otomura Kazutoshi <otomura.kazutoshi@gmail.com>
2014-08-02 15:56:44 +0900

    LocalStrage対応

--- js/app.js
+++ js/app.js
@@ -1,9 +1,5 @@
 var app = app || {};
 
 $(function() {
-	var cars = [
-		{carSpec: '•’Ê‚Ì©‰Æ—pÔ', maxNum: 4, charge: 10000},
-		{carSpec: '•’Ê‚ÌƒoƒX', maxNum: 20, charge: 30000}
-	];
-	new app.CarCollectionView(cars);
+	new app.CarCollectionView();
 });
