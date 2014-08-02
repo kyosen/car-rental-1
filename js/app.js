553938cc38affdbb52575c80ed05a1337bed8d84
Otomura Kazutoshi <otomura.kazutoshi@gmail.com>
2014-08-02 14:31:14 +0900

    app修正

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 var app = app || {};
 
 $(function() {
-	var cars = {
+	var cars = [
 		{carSpec: '•’Ê‚Ì©‰Æ—pÔ', maxNum: 4, charge: 10000},
 		{carSpec: '•’Ê‚ÌƒoƒX', maxNum: 20, charge: 30000}
-	};
-	new app.carCollectionView(cars);
-};)
+	];
+	new app.CarCollectionView(cars);
+});
